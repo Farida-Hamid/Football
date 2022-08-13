5bade0a4273b790957d033fd6f5f09955a95584f
Farida-Hamid <faridaaddala@gmail.com>
2022-08-13 10:50:55 +0300

    Style header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NavLink, useLocation } from "react-router-dom";
-import mainLogo from "./image/home.png";
 import MicIcon from '@mui/icons-material/Mic';
 import SettingsIcon from '@mui/icons-material/Settings';
 import './style/header.css';
@@ -8,26 +7,26 @@ import './style/header.css';
 function Header() {
   const location = useLocation();
   const { pathname } = location;
-  const splitLocation = pathname.split('/');
+  const splitLocation = pathname.split('/')[1];
   const back = '<';
 
   return (
     <nav>
-      <div className='links'>
+      <div className='link'>
         <NavLink
           to="/"
-          className={splitLocation[1] === '' ? 'active' : 'inactive'}
+          className={splitLocation}
           >
           {back}
         </NavLink>
       </div>
       <div className='home'>
-        <img  src={mainLogo} style={ { width: '40px', height: '30px' } } alt="footbal logo"/>
         <h1>Football Leagues</h1>
-        <img  src={mainLogo} style={ { width: '40px', height: '30px' } } alt="footbal logo"/>
       </div>
-      <MicIcon />
-      <SettingsIcon />
+      <div>
+        <MicIcon />
+        <SettingsIcon />
+      </div>
     </nav>
   );
 }
