7f1fed01760b1520fc8e4f0b87962babe18019ea
Farida-Hamid <faridaaddala@gmail.com>
2022-08-13 09:59:34 +0300

    Add functionality to header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-// import { NavLink, useLocation } from "react-router-dom";
-// import mainLogo from "../image/home.png";
+import { NavLink, useLocation } from "react-router-dom";
 // import './header.css';
 
 function Header() {
-  // const location = useLocation();
-  // const { pathname } = location;
-  // const splitLocation = pathname.split('/');
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split('/');
+  const back = '<';
 
   return (
     <nav>
-      <div className='home'>
-        {/* <img  src={mainLogo} style={ { width: '50px' } } alt="home"/> */}
-        <h1>Football Leagues</h1>
-      </div>
       <div className='links'>
-        {/* <NavLink
+        <NavLink
           to="/"
           className={splitLocation[1] === '' ? 'active' : 'inactive'}
           >
-          Rockets
-        </NavLink> */}
-        links
+          {back}
+        </NavLink>
+      </div>
+      <div className='home'>
+        <h1>Football Leagues</h1>
       </div>
     </nav>
   );
